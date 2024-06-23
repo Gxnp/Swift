a5b4731cb749aa6f337c662503c1c3947232605a
Gxnp <guynopan@gmail.com>
2024-06-23 10:16:04 +0700

    test

---
+++ DemoGithub/DemoGithub/DemoGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubApp.swift
+//  DemoGithub
+//
+//  Created by napon narkphan on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
